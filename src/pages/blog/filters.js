9fc4e8ed2d00edb2b833bd74b009631c18324548
Denis Savinov <kils@list.ru>
2021-08-02 17:33:26 +0400

    Продолжение ДЗ - 8

---
+++ src/pages/blog/filters.js
@@ -0,0 +1,11 @@
+(function() {
+    getParams ();
+    function setDataToFilter () {
+
+    };
+})();
+
+function getParams () {
+    let searchParams = new URLSearchParams(location.search);
+    console.log(searchParams.getAll('comments-item-checkbox'));
+}
