c975744c4bad2d0cea79952928e352b05e42d7c2
dn1984 <kils@list.ru>
2021-08-23 01:13:22 +0400

    оптимизация, финальная проверка

--- src/pages/main/script.js
+++ src/pages/main/script.js
@@ -1,3 +1,6 @@
+
+const SERVER_URL = 'https://academy.directlinedev.com';
+
 renderMenu ();
 
 //button to top pages
@@ -815,5 +818,39 @@ function visualMessageSendForm(successOrError, form = null) {
             errorMessage.classList.add('hidden');
             overlay.classList.add('hidden');
         }, 2000)
+    }
+}
+
+function sendrequest ({url, method = 'GET', headers, body = null}) {
+    return fetch(SERVER_URL + url + '?v=1.0.0', {
+        method,
+        headers,
+        body,
+    })
+}
+
+function renderMenu () {
+    const noLoginLinks = document.querySelectorAll('.nav-no-login_js');
+    const loginLinks = document.querySelectorAll('.nav-login_js');
+    const isLogin = localStorage.getItem('token');
+
+    if (!noLoginLinks || !loginLinks) {
+        return;
+    }
+
+    if (isLogin) {
+        loginLinks.forEach(link => {
+            link.classList.remove('hidden');
+        })
+        noLoginLinks.forEach(link => {
+            link.classList.add('hidden');
+        })
+    } else {
+        loginLinks.forEach(link => {
+            link.classList.add('hidden');
+        })
+        noLoginLinks.forEach(link => {
+            link.classList.remove('hidden');
+        })
     }
 }
